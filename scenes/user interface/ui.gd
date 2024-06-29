81de4c903d79d32ce3dad9ef80e35e0b49da2810
RomuloDurante <romulodurante@protonmail.com>
2024-06-29 08:10:33 -0300

    change color label

--- scenes/user interface/ui.gd
+++ scenes/user interface/ui.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+#colors
+
+var green:Color = Color("6bbfa3")
+var red:Color = Color(0.9,0,0,1)
+
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label
-@onready var label_grenade = $GrenadeCounter/VBoxContainer/Label
+@onready var laser_icon:TextureRect = $LaserCounter/VBoxContainer/TextureRect
+
+@onready var grenade_label:Label = $GrenadeCounter/VBoxContainer/Label
+@onready var grenade_icon:TextureRect = $GrenadeCounter/VBoxContainer/TextureRect
+
 
 
 func _ready():
@@ -11,6 +20,16 @@ func _ready():
 
 func update_laser_text():
 	laser_label.text = str(Globals.laser_ammount)
+	update_color(Globals.laser_ammount, laser_label, laser_icon)
 	
 func update_greande_text():
-	label_grenade.text = str(Globals.grenade_ammount)
+	grenade_label.text = str(Globals.grenade_ammount)
+	update_color(Globals.grenade_ammount, grenade_label, grenade_icon)
+	
+func update_color(ammount:int, label:Label, icon:TextureRect) -> void:
+	label.modulate = green
+	icon.modulate = green
+	if ammount == 0:
+		label.modulate = red
+		icon.modulate = red
+		
